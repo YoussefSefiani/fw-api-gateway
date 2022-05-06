d1f8321efb0e130335b1349f2dde5a708c87451a
YoussefSefiani <youssef.sefiani@student.ehb.be>
2022-05-06 17:39:45 +0200

    webconfig

--- src/main/java/fw/apigateway/config/WebConfiguration.java
+++ src/main/java/fw/apigateway/config/WebConfiguration.java
@@ -1,26 +1,26 @@
-package fw.apigateway.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
-
-@Configuration
-public class WebConfiguration implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://fw-front-end.herokuapp.com")
-                .exposedHeaders("*")
-                .allowedMethods("*")
-                .allowedHeaders("*");
-    }
-
-}
+//package fw.apigateway.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import java.util.Arrays;
+//
+//@Configuration
+//public class WebConfiguration implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("https://fw-front-end.herokuapp.com")
+//                .exposedHeaders("*")
+//                .allowedMethods("*")
+//                .allowedHeaders("*");
+//    }
+//
+//}
